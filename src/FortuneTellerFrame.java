1027ccdd162eab2bae701b34cf39628a8dc32137
koopmanjoseph115-code <koopmanjoseph115@gmail.com>
2026-09-24 15:54:17 -0400

    int

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -5,9 +5,11 @@ public class FortuneTellerFrame extends JFrame {
 
     public FortuneTellerFrame(){
         JPanel toppanel = new JPanel();
-        ImageIcon fortuneimg = new ImageIcon("RES/FortuneTeller_img.png");
+        ImageIcon fortuneimg = new ImageIcon("RES/FortuneTeller_img.png", );
         JLabel titlelabel = new JLabel("FortuneTeller", fortuneimg, JLabel.CENTER);
         titlelabel.setFont(new Font("Serif", Font.BOLD, 48));
+        titlelabel.setHorizontalTextPosition(JLabel.CENTER);
+        titlelabel.setVerticalTextPosition(JLabel.BOTTOM);
         toppanel.add(titlelabel);
 
         JPanel middlepanel = new JPanel();
@@ -24,6 +26,11 @@ public class FortuneTellerFrame extends JFrame {
         bottompanel.add(fortunebutton);
         bottompanel.add(quitbutton);
 
+        setLayout(new BorderLayout(10,10));
+        add(toppanel, BorderLayout.NORTH);
+        add(middlepanel, BorderLayout.CENTER);
+        add(bottompanel, BorderLayout.SOUTH);
 
+        setSize(1000,1000);
     }
 }
