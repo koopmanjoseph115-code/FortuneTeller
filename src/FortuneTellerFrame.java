da3819c9c97413f06b0e6eb6675c5646d8638a79
koopmanjoseph115-code <koopmanjoseph115@gmail.com>
2026-09-24 16:20:45 -0400

    int

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class FortuneTellerFrame extends JFrame {
 
     private String[] fortunes = {
             "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"
-    }
+    };
+    private int previousfortune = -1;
 
     public FortuneTellerFrame(){
         JPanel toppanel = new JPanel();
@@ -17,10 +19,11 @@ public class FortuneTellerFrame extends JFrame {
         toppanel.add(titlelabel);
 
         JPanel middlepanel = new JPanel();
-        JTextArea fortunetextarea = new JTextArea();
+        JTextArea fortunetextarea = new JTextArea(10, 25);
         fortunetextarea.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        fortunetextarea.setLineWrap(true);
         JScrollPane scrollpane = new JScrollPane(fortunetextarea);
-        middlepanel.add(scrollpane);
+        middlepanel.add(scrollpane,BorderLayout.CENTER);
 
         JPanel bottompanel = new JPanel();
         JButton fortunebutton = new JButton("Read My Fortune!");
@@ -30,11 +33,32 @@ public class FortuneTellerFrame extends JFrame {
         bottompanel.add(fortunebutton);
         bottompanel.add(quitbutton);
 
+        fortunebutton.addActionListener(
+                e ->{
+                    Random random = new Random();
+                    int currentfortune;
+                    do {
+                        currentfortune = random.nextInt(fortunes.length);
+                    }
+                    while (currentfortune == previousfortune);
+                    previousfortune = currentfortune;
+                    fortunetextarea.append(fortunes[currentfortune] + "\n");
+                    System.out.println(fortunes[currentfortune]);
+                }
+        );
+
+        quitbutton.addActionListener(e -> {System.exit(0);});
+
+
         setLayout(new BorderLayout(10,10));
         add(toppanel, BorderLayout.NORTH);
         add(middlepanel, BorderLayout.CENTER);
         add(bottompanel, BorderLayout.SOUTH);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screensize = toolkit.getScreenSize();
+        int width = (int)(screensize.width * .75);
+        int height = (int)(screensize.height * .75);
 
-        setSize(1000,1000);
+        setSize(width, height);
     }
 }
